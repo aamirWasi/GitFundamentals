4eaa9b30e3442f3aa4be87dd05ec597ab9f56702
aamirWasi <46648085+aamirWasi@users.noreply.github.com>
2019-07-30 02:23:09 +0600

    Update Program.cs

--- GitSecondProject/Program.cs
+++ GitSecondProject/Program.cs
@@ -17,6 +17,13 @@ namespace GitSecondProject
                 LastName = "Morshed",
                 Age = 23
             };
+            var person2 = new
+            {
+                FirstName = "aamirWasi",
+                LastName = "Morshed",
+                Age = 23,
+                Salary=35000
+            };
             Console.WriteLine($"{person.FirstName} {person.LastName}");
             //Code to call at DevAamir
             //Added a console
