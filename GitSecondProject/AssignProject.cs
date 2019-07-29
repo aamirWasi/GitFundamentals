498fc556083ae9f054dd4920cfc3f2d63df0753e
aamir <wasi3aamir@gmail.com>
2019-07-29 22:20:32 +0600

    Added a method in AssignProject

--- GitSecondProject/AssignProject.cs
+++ GitSecondProject/AssignProject.cs
@@ -3,5 +3,12 @@
     public class AssignProject
     {
         //This is important for every group members
+        public int Add()
+        {
+            int x = 1;
+            int y = 2;
+            int sum = x + y;
+            return sum;
+        }
     }
 }
