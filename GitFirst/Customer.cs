d11708f6f58f60ed2d400d91b89de659b8915313
aamir <wasi3aamir@gmail.com>
2019-07-29 14:57:12 +0600

    Added GetBonusMethod in Customer class

--- GitFirst/Customer.cs
+++ GitFirst/Customer.cs
@@ -5,6 +5,9 @@
         public string Name { get; set; }
         public int ID { get; set; }
         public double Salary { get; set; }
-        
+        public double GetBonus()
+        {
+            return 0.15 * Salary;
+        }
     }
 }
