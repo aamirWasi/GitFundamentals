4da036a9c881acc24513420b39d2cba87a06ef3f
aamir <wasi3aamir@gmail.com>
2019-07-29 14:53:13 +0600

    Added Customer's Credential

--- GitFirst/Customer.cs
+++ GitFirst/Customer.cs
@@ -2,6 +2,12 @@
 {
     public class Customer
     {
-
+        public string Name { get; set; }
+        public int ID { get; set; }
+        public double Salary { get; set; }
+        public void Bonus(double salary)
+        {
+            Salary = salary;
+        }
     }
 }
