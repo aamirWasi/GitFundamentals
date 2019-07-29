b05eda9b78d57b8d8984c9eeb7a0903780ec30ed
aamir <wasi3aamir@gmail.com>
2019-07-29 14:41:55 +0600

    Added Add Method

--- GitFirst/Feature1.cs
+++ GitFirst/Feature1.cs
@@ -2,6 +2,11 @@
 {
     public class Feature1
     {
-
+        public int Add()
+        {
+            int x = 1;int y = 2;
+            int sum = x + y;
+            return sum;
+        }
     }
 }
